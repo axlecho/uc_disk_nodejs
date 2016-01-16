bdacb24b1b9e8873083abbb938d55b5c2f9be4c7
axlecho <axlecho@gmail.com>
2016-01-17 01:00:02 +0800

    完成api的施工

--- api.js
+++ api.js
@@ -1,10 +1,16 @@
 var http = require('http');
 var qs = require('querystring');  
 var fs = require('fs')
-var debug = true;
+var debug = false;
 var silent = false;
 
-var cookieManger = {};
+var cookieManger = {
+	//'mode':'list',
+	//'netdisk_vip':'LTE=',
+	//'s_uid':'7200ED8C38'
+};
+
+var serviceTicket = "";
 
 function getTimeStamp() {
 	return new Date().getTime();
@@ -40,8 +46,13 @@ function getCaptchaId(cb) {
 			console.log('[getCaptchaId] status:' + statusCode);
 		}
 		
+		var chunks = [];
 		res.on('data', function (chunk) {  
-			cb(chunk);
+			  chunks.push(chunk);
+		});
+		
+		res.on('end',function() {
+			cb(chunks);
 		});
 	});
 
@@ -124,8 +135,13 @@ function login(name,pass,code,codeId,cb) {
 		if(!silent) {
 			console.log('[login] status:' + statusCode);
 		}
+		var chunks = [];
 		res.on('data', function (chunk) {  
-			cb(chunk);
+			  chunks.push(chunk);
+		});
+		
+		res.on('end',function() {
+			cb(chunks);
 		});
 	});
 	
@@ -163,9 +179,262 @@ function praseCookie(cookies) {
 	}
 }
 
+function praseServiceTicket(webdata) {
+	var scriptRep = new RegExp('(response=).*(";)');
+	var responseString  = scriptRep.exec(webdata);
 
+	if(debug) {
+		console.log('======= [praseServiceTicket] webdata =======');
+		console.log(webdata);
+		console.log('=============================================');
+		console.log('[praseServiceTicket] response:' + responseString[0]);
+	}
+	
+	var response = JSON.parse(responseString[0].split('=')[1]
+		.replace(new RegExp(/";/),''));
+	if(debug) {
+		console.log('======= [praseServiceTicket] responseObject =======');
+		console.log(response);
+		console.log('=============================================');
+	}
+	if(response.status == 20000) {
+		serviceTicket = response.data.service_ticket;
+	}
+	if(debug) {
+		console.log('[praseServiceTicket] serviceTicket:' + serviceTicket);
+	}
+}
 
+function getDirInfo(dir,cb) {
+	var hostname = 'disk.yun.uc.cn';
+	var urlparam = {
+		dirid:dir,
+		type:'all',
+		_:getTimeStamp()
+	};
+	
+	var path = '/netdisk/ajaxnd?' +  qs.stringify(urlparam);
+	
+	if (debug) {
+		console.log('[getDirInfo] url:' + hostname + path);
+	}
+	
+	var cookie = '';
+	for ( var field in cookieManger ){
+		cookie += field + '=' + cookieManger[field] + ';';
+	}
+	var options = {
+		hostname:hostname,
+		path:path,
+		method:'get',
+		headers: {  
+			'Accept':'application/json, text/javascript, */*; q=0.01',
+			'Accept-Encoding':'gzip, deflate, sdch',
+			'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
+			'Cache-Control':'no-cache',
+			'Connection':'keep-alive',
+            'Content-Type': 'application/json', 
+			'Pragma':'no-cache',
+			'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
+			'X-Requested-With':'XMLHttpRequest',
+			"Cookie": cookie
+        }
+	};
+	if(debug) {
+		console.log('[getDirInfo] headers cookie:' + cookie);
+	}
+	var req = http.request(options,function(res) {
+		var statusCode = res.statusCode;
+		var headers = JSON.parse(JSON.stringify(res.headers));
+		var cookies = headers['set-cookie'];
+		if(debug) {
+			console.log('======= [getDirInfo] headers =======');
+			console.log(headers);
+			console.log('=============================================');
+		}
+		if(!silent) {
+			console.log('[getDirInfo] status:' + statusCode);
+		}
+		
+		var chunks = [];
+		res.on('data', function (chunk) {  
+			  chunks.push(chunk);
+		});
+		
+		res.on('end',function() {
+			cb(chunks);
+		});
+	});
+
+	req.on('error', function (e) {
+		console.log('[getCaptcha] problem with request:' + e.message);
+	});
+	
+	req.end();
+	
+}
+
+function setServiceTicket(cb) {
+	var hostname = 'yun.uc.cn';
+	
+	var urlparam = {
+		service_ticket:serviceTicket
+	};
+	
+	var path = '/exter/basicinfo?' + qs.stringify(urlparam);
+		
+	if (debug) {
+		console.log('[setServiceTicket] url:' + hostname + path);
+	}
+	
+	var options = {
+		hostname:hostname,
+		path:path,
+		method:'get'
+	};
+	
+	var req = http.request(options,function(res) {
+		var statusCode = res.statusCode;
+		var headers = JSON.parse(JSON.stringify(res.headers));
+		var cookies = headers['set-cookie'];
+		praseCookie(cookies);
+		if(!silent) {
+			console.log('[setServiceTicket] status:' + statusCode);
+		}
+		
+		var chunks = [];
+		res.on('data', function (chunk) {  
+			  chunks.push(chunk);
+		});
+		
+		res.on('end',function() {
+			cb(chunks);
+		});
+	});
+
+	req.on('error', function (e) {
+		console.log('[setServiceTicket] problem with request: ' + e.message);
+	});
+	
+	req.end();
+}
+
+function setLn(cb) {
+	var hostname = 'yun.uc.cn';
+	var path = '/index.php/netdisk_service/ajax/setLn';
+	if(debug) {
+		console.log('[setLn] url:' + hostname + path);
+	}
+	
+	var options = {
+		hostname: hostname,
+		path:path,
+		method: 'POST',
+		headers: {  
+            "Content-Type": 'application/x-www-form-urlencoded'
+        }  
+	};
+	
+	var req = http.request(options,function(res) {
+		var statusCode = res.statusCode;
+		var headers = JSON.parse(JSON.stringify(res.headers));
+		var cookies = headers['set-cookie'];
+		praseCookie(cookies);
+		if(!silent) {
+			console.log('[setLn] status:' + statusCode);
+		}
+		cb();
+	});
+	
+	req.on('error', function (e) {
+		console.log('[setLn] problem with request:' + e.message);
+	});
+
+	
+	var content = {
+		ln:"axlecho@126.com"
+	};
+	var postdata = qs.stringify(content);
+	
+	if(debug) {
+	    console.log('[setLn] post data:' + postdata);
+	}
+	req.write(postdata +  '\n');
+	req.end();
+}
+
+function getIndex(cb) {
+	var hostname = 'disk.yun.uc.cn';
+	
+	var path = '/';
+		
+	if (debug) {
+		console.log('[getIndex] url:' + hostname + path);
+	}
+	
+	var cookie = '';
+	for ( var field in cookieManger ){
+		cookie += field + '=' + cookieManger[field] + ';';
+	}
+	var options = {
+		hostname:hostname,
+		path:path,
+		method:'get',
+		headers: {  
+			'Accept':'application/json, text/javascript, */*; q=0.01',
+			'Accept-Encoding':'gzip, deflate, sdch',
+			'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
+			'Cache-Control':'no-cache',
+			'Connection':'keep-alive',
+            'Content-Type': 'application/json', 
+			'Pragma':'no-cache',
+			'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
+			'X-Requested-With':'XMLHttpRequest',
+			"Cookie": cookie
+        }
+	};
+	if(debug) {
+		console.log('[getIndex] headers cookie:' + cookie);
+	}
+	
+	var req = http.request(options,function(res) {
+		var statusCode = res.statusCode;
+		var headers = JSON.parse(JSON.stringify(res.headers));
+		var cookies = headers['set-cookie'];
+		praseCookie(cookies);
+		
+		if(debug) {
+			console.log('======= [getIndex] headers =======');
+			console.log(headers);
+			console.log('=============================================');
+		}
+		
+		if(!silent) {
+			console.log('[getIndex] status:' + statusCode);
+		}
+		
+		var chunks = [];
+		res.on('data', function (chunk) {  
+			  chunks.push(chunk);
+		});
+		
+		res.on('end',function() {
+			cb(chunks);
+		});
+	});
+
+	req.on('error', function (e) {
+		console.log('[getIndex] problem with request: ' + e.message);
+	});
+	
+	req.end();
+}
 
 exports.login = login;
 exports.getCaptchaId = getCaptchaId;
 exports.getCaptcha = getCaptcha;
+exports.praseServiceTicket = praseServiceTicket;
+exports.getDirInfo = getDirInfo;
+exports.setServiceTicket = setServiceTicket;
+exports.setLn = setLn;
+exports.getIndex = getIndex;
--- testapi.js
+++ testapi.js
@@ -1,5 +1,6 @@
 var api = require('./api.js');
 var mCaptchaId = '';
+var zlib = require('zlib');
 
 api.getCaptchaId(function (captchaId) {
 	captchaId  = captchaId.toString().split(':')[1]
@@ -18,13 +19,29 @@ process.stdin.on('readable', function() {
 	if (captcha == null || captcha == undefined || captcha == '') { 
 		return;
 	}
+
 	captcha = captcha.replace(/\s+/g, '')
 	var user = 'axlecho@126.com';
 	var pass = '!me433978029';
 	
 	api.login(user,pass,captcha,mCaptchaId,function(data) {
-		console.log(decodeURIComponent(data.toString()));
+		api.praseServiceTicket(decodeURIComponent(data.toString()));
+		api.setServiceTicket(function(data) {
+			console.log(data.toString());
+			api.setLn(function() {
+				api.getDirInfo('1',function(data){
+					var buffer = Buffer.concat(data);
+					zlib.gunzip(buffer, function (err, decoded) {
+						console.log(decoded.toString());
+					});
+				});
+			});
+		});
 	});
+	
+	process.stdin.resume();
+	process.stdin.pause();
+	process.stdin.destroy();
 });
 
 process.stdin.on('end', function() {
